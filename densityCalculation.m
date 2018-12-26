05288026de8b52dabf3d83e53fa0171b833c0e58
OldPig <angleoldpig@gmail.com>
2018-12-27 02:36:13 +0800

    加入Reset键，改用选项卡界面

--- densityCalculation.m
+++ densityCalculation.m
@@ -3,25 +3,27 @@ classdef densityCalculation < matlab.apps.AppBase
     % Properties that correspond to app components
     properties (Access = public)
         UIFigure                        matlab.ui.Figure
-        FunctionButtonGroup             matlab.ui.container.ButtonGroup
-        MoistAirButton                  matlab.ui.control.RadioButton
-        NAButton                        matlab.ui.control.RadioButton
-        NAAButton                       matlab.ui.control.RadioButton
+        TabGroup                        matlab.ui.container.TabGroup
+        MoistAirTab                     matlab.ui.container.Tab
         ResetButton                     matlab.ui.control.Button
         CalculateButton                 matlab.ui.control.Button
-        ambienttemperatureCEditFieldLabel  matlab.ui.control.Label
+        FunctionTextAreaLabel           matlab.ui.control.Label
+        FunctionTextArea                matlab.ui.control.TextArea
+        ambienttemperatureCEditFieldLabel_2  matlab.ui.control.Label
         ambienttemperatureCEditField    matlab.ui.control.NumericEditField
         relativehumidityEditFieldLabel  matlab.ui.control.Label
         relativehumidityEditField       matlab.ui.control.NumericEditField
-        ambientpressurePaEditFieldLabel  matlab.ui.control.Label
+        ambientpressurePaEditFieldLabel_2  matlab.ui.control.Label
         ambientpressurePaEditField      matlab.ui.control.NumericEditField
-        FunctionTextAreaLabel           matlab.ui.control.Label
-        FunctionTextArea                matlab.ui.control.TextArea
-        InputLabel                      matlab.ui.control.Label
-        OutPutLabel                     matlab.ui.control.Label
         densitykgm3EditFieldLabel       matlab.ui.control.Label
         densitykgm3EditField            matlab.ui.control.NumericEditField
+        InputLabel                      matlab.ui.control.Label
+        OutputLabel                     matlab.ui.control.Label
+        Tab2                            matlab.ui.container.Tab
+        Tab3                            matlab.ui.container.Tab
     end
+
+
 
     methods (Access = private)
 
@@ -57,10 +59,10 @@ classdef densityCalculation < matlab.apps.AppBase
             Mv = 18.015*10^-3;        % Molar mass of water vapour [kg/mol]
             Ma = 28.9635*10^-3;       % Molar mass of dry air      [kg/mol]
             
-            A =  1.2378847*10^-5;    % [^-2]
-            B = -1.9121316*10^-2;    % [^-1]
-            C = 33.93711047;         %
-            D = -6.3431645*10^3;     % []
+             A =  1.2378847*10^-5;    % [^-2]
+             B = -1.9121316*10^-2;    % [^-1]
+             C = 33.93711047;         %
+             D = -6.3431645*10^3;     % []
             
             a0 =  1.58123*10^-6;      % [/Pa]
             a1 = -2.9331*10^-8;       % [1/Pa]
@@ -105,6 +107,14 @@ classdef densityCalculation < matlab.apps.AppBase
             app.densitykgm3EditField.Value = ro;
             
         end
+
+        % Button pushed function: ResetButton
+        function ResetButtonPushed(app, event)
+            app.ambienttemperatureCEditField.Value = 0;
+            app.relativehumidityEditField.Value = 0;
+            app.ambientpressurePaEditField.Value = 0;
+            app.densitykgm3EditField.Value = 0;
+        end
     end
 
     % App initialization and construction
@@ -115,104 +125,101 @@ classdef densityCalculation < matlab.apps.AppBase
 
             % Create UIFigure
             app.UIFigure = uifigure;
-            app.UIFigure.Position = [100 100 640 480];
+            app.UIFigure.Position = [100 100 671 643];
             app.UIFigure.Name = 'UI Figure';
 
-            % Create FunctionButtonGroup
-            app.FunctionButtonGroup = uibuttongroup(app.UIFigure);
-            app.FunctionButtonGroup.Title = 'Function';
-            app.FunctionButtonGroup.Position = [126 248 123 106];
+            % Create TabGroup
+            app.TabGroup = uitabgroup(app.UIFigure);
+            app.TabGroup.Position = [35 101 604 460];
 
-            % Create MoistAirButton
-            app.MoistAirButton = uiradiobutton(app.FunctionButtonGroup);
-            app.MoistAirButton.Text = 'Moist Air';
-            app.MoistAirButton.Position = [11 60 69 22];
-            app.MoistAirButton.Value = true;
-
-            % Create NAButton
-            app.NAButton = uiradiobutton(app.FunctionButtonGroup);
-            app.NAButton.Text = 'NA';
-            app.NAButton.Position = [11 38 39 22];
-
-            % Create NAAButton
-            app.NAAButton = uiradiobutton(app.FunctionButtonGroup);
-            app.NAAButton.Text = 'NAA';
-            app.NAAButton.Position = [11 16 47 22];
+            % Create MoistAirTab
+            app.MoistAirTab = uitab(app.TabGroup);
+            app.MoistAirTab.Title = 'Moist Air';
 
             % Create ResetButton
-            app.ResetButton = uibutton(app.UIFigure, 'push');
-            app.ResetButton.Position = [175 40 100 22];
+            app.ResetButton = uibutton(app.MoistAirTab, 'push');
+            app.ResetButton.ButtonPushedFcn = createCallbackFcn(app, @ResetButtonPushed, true);
+            app.ResetButton.Position = [173 50 100 22];
             app.ResetButton.Text = 'Reset';
 
             % Create CalculateButton
-            app.CalculateButton = uibutton(app.UIFigure, 'push');
+            app.CalculateButton = uibutton(app.MoistAirTab, 'push');
             app.CalculateButton.ButtonPushedFcn = createCallbackFcn(app, @CalculateButtonPushed, true);
-            app.CalculateButton.Position = [401 40 100 22];
+            app.CalculateButton.Position = [348 50 100 22];
             app.CalculateButton.Text = 'Calculate';
 
-            % Create ambienttemperatureCEditFieldLabel
-            app.ambienttemperatureCEditFieldLabel = uilabel(app.UIFigure);
-            app.ambienttemperatureCEditFieldLabel.HorizontalAlignment = 'right';
-            app.ambienttemperatureCEditFieldLabel.Position = [56 211 135 22];
-            app.ambienttemperatureCEditFieldLabel.Text = 'ambient temperature [C]';
-
-            % Create ambienttemperatureCEditField
-            app.ambienttemperatureCEditField = uieditfield(app.UIFigure, 'numeric');
-            app.ambienttemperatureCEditField.Position = [206 211 100 22];
-
-            % Create relativehumidityEditFieldLabel
-            app.relativehumidityEditFieldLabel = uilabel(app.UIFigure);
-            app.relativehumidityEditFieldLabel.HorizontalAlignment = 'right';
-            app.relativehumidityEditFieldLabel.Position = [78 173 113 22];
-            app.relativehumidityEditFieldLabel.Text = 'relative humidity [%]';
-
-            % Create relativehumidityEditField
-            app.relativehumidityEditField = uieditfield(app.UIFigure, 'numeric');
-            app.relativehumidityEditField.Position = [206 173 100 22];
-
-            % Create ambientpressurePaEditFieldLabel
-            app.ambientpressurePaEditFieldLabel = uilabel(app.UIFigure);
-            app.ambientpressurePaEditFieldLabel.HorizontalAlignment = 'right';
-            app.ambientpressurePaEditFieldLabel.Position = [68 133 123 22];
-            app.ambientpressurePaEditFieldLabel.Text = 'ambient pressure [Pa]';
-
-            % Create ambientpressurePaEditField
-            app.ambientpressurePaEditField = uieditfield(app.UIFigure, 'numeric');
-            app.ambientpressurePaEditField.Position = [206 133 100 22];
-
             % Create FunctionTextAreaLabel
-            app.FunctionTextAreaLabel = uilabel(app.UIFigure);
+            app.FunctionTextAreaLabel = uilabel(app.MoistAirTab);
             app.FunctionTextAreaLabel.HorizontalAlignment = 'right';
-            app.FunctionTextAreaLabel.Position = [362 307 52 22];
+            app.FunctionTextAreaLabel.Position = [340 236 52 22];
             app.FunctionTextAreaLabel.Text = 'Function';
 
             % Create FunctionTextArea
-            app.FunctionTextArea = uitextarea(app.UIFigure);
-            app.FunctionTextArea.Position = [429 271 150 60];
+            app.FunctionTextArea = uitextarea(app.MoistAirTab);
+            app.FunctionTextArea.Position = [425 200 132 60];
+
+            % Create ambienttemperatureCEditFieldLabel_2
+            app.ambienttemperatureCEditFieldLabel_2 = uilabel(app.MoistAirTab);
+            app.ambienttemperatureCEditFieldLabel_2.HorizontalAlignment = 'right';
+            app.ambienttemperatureCEditFieldLabel_2.Position = [39 238 135 22];
+            app.ambienttemperatureCEditFieldLabel_2.Text = 'ambient temperature [C]';
+
+            % Create ambienttemperatureCEditField
+            app.ambienttemperatureCEditField = uieditfield(app.MoistAirTab, 'numeric');
+            app.ambienttemperatureCEditField.Position = [189 238 100 22];
+
+            % Create relativehumidityEditFieldLabel
+            app.relativehumidityEditFieldLabel = uilabel(app.MoistAirTab);
+            app.relativehumidityEditFieldLabel.HorizontalAlignment = 'right';
+            app.relativehumidityEditFieldLabel.Position = [61 200 113 22];
+            app.relativehumidityEditFieldLabel.Text = 'relative humidity [%]';
+
+            % Create relativehumidityEditField
+            app.relativehumidityEditField = uieditfield(app.MoistAirTab, 'numeric');
+            app.relativehumidityEditField.Position = [189 200 100 22];
+
+            % Create ambientpressurePaEditFieldLabel_2
+            app.ambientpressurePaEditFieldLabel_2 = uilabel(app.MoistAirTab);
+            app.ambientpressurePaEditFieldLabel_2.HorizontalAlignment = 'right';
+            app.ambientpressurePaEditFieldLabel_2.Position = [51 160 123 22];
+            app.ambientpressurePaEditFieldLabel_2.Text = 'ambient pressure [Pa]';
+
+            % Create ambientpressurePaEditField
+            app.ambientpressurePaEditField = uieditfield(app.MoistAirTab, 'numeric');
+            app.ambientpressurePaEditField.Position = [189 160 100 22];
 
             % Create densitykgm3EditFieldLabel
-            app.densitykgm3EditFieldLabel = uilabel(app.UIFigure);
+            app.densitykgm3EditFieldLabel = uilabel(app.MoistAirTab);
             app.densitykgm3EditFieldLabel.HorizontalAlignment = 'right';
-            app.densitykgm3EditFieldLabel.Position = [370 190 86 22];
+            app.densitykgm3EditFieldLabel.Position = [325 160 86 22];
             app.densitykgm3EditFieldLabel.Text = 'density [kg/m3]';
 
             % Create densitykgm3EditField
-            app.densitykgm3EditField = uieditfield(app.UIFigure, 'numeric');
-            app.densitykgm3EditField.Position = [471 190 100 22];
+            app.densitykgm3EditField = uieditfield(app.MoistAirTab, 'numeric');
+            app.densitykgm3EditField.Position = [426 160 131 22];
 
             % Create InputLabel
-            app.InputLabel = uilabel(app.UIFigure);
-            app.InputLabel.FontSize = 40;
+            app.InputLabel = uilabel(app.MoistAirTab);
+            app.InputLabel.FontSize = 36;
             app.InputLabel.FontWeight = 'bold';
-            app.InputLabel.Position = [136 388 103 48];
-            app.InputLabel.Text = 'Input';
+            app.InputLabel.Position = [129 318 93 45];
+            app.InputLabel.Text = {'Input'; ''};
 
-            % Create OutPutLabel
-            app.OutPutLabel = uilabel(app.UIFigure);
-            app.OutPutLabel.FontSize = 40;
-            app.OutPutLabel.FontWeight = 'bold';
-            app.OutPutLabel.Position = [401 388 139 48];
-            app.OutPutLabel.Text = 'OutPut';
+            % Create OutputLabel
+            app.OutputLabel = uilabel(app.MoistAirTab);
+            app.OutputLabel.HorizontalAlignment = 'center';
+            app.OutputLabel.FontSize = 36;
+            app.OutputLabel.FontWeight = 'bold';
+            app.OutputLabel.Position = [380 318 123 45];
+            app.OutputLabel.Text = {'Output'; ''};
+
+            % Create Tab2
+            app.Tab2 = uitab(app.TabGroup);
+            app.Tab2.Title = 'Tab2';
+
+            % Create Tab3
+            app.Tab3 = uitab(app.TabGroup);
+            app.Tab3.Title = 'Tab3';
         end
     end
 
